844798d2231a976b81297fcf44cf6809f9b1eddf
FindMalek <malekgarahellalbus@gmail.com>
2023-09-25 00:32:29 +0100

    🚀 Initial commit: Introducing the background.

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css'
+
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Home | FindServices',
+  description: 'FindServices | Find services near you, anywhere in the world. This is me Malek Gara-Hellal and I am a full-stack developer.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
