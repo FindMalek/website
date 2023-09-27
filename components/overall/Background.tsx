e3fe12faa57a19a14f873c939c9b99fcf5547ab8
FindMalek <malekgarahellalbus@gmail.com>
2023-09-27 12:00:44 +0100

    ⚡ Improve performance: 'aria-label' for CloudLogos.

--- components/overall/Background.tsx
+++ components/overall/Background.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { Old_Standard_TT } from "next/font/google";
 
 const purplePurse = Old_Standard_TT({
-  weight: ["400", "700"],
+  weight: ["700"],
   subsets: ["latin"],
 });
 
@@ -85,7 +85,7 @@ export default function Background() {
           }}
         />
         <div
-          className="aspect-[200/45] w-[20.25rem] bg-gradient-to-r from-[#ff02fb] to-[#00ffff] opacity-90"
+          className="aspect-[200/45] w-[20.25rem] bg-gradient-to-r from-[#3002ff] to-[#00ffff] opacity-90"
           style={{
             clipPath:
               "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
--- components/sections/footer/CloudLogos.tsx
+++ components/sections/footer/CloudLogos.tsx
@@ -11,43 +11,77 @@ export default function CloudLogos() {
   return (
     <>
       <div className="hidden md:flex space-x-5 pt-2">
-        <Link href="https://www.instagram.com/findmalek" target="_blank">
+        <Link
+          href="https://www.instagram.com/findmalek"
+          target="_blank"
+          aria-label="Instagram"
+        >
           <BiLogoInstagram className="h-5 w-5" />
         </Link>
         <Link
           href="https://www.linkedin.com/in/malek-gara-hellal-a2936221a/"
           target="_blank"
+          aria-label="LinkedIn"
         >
           <BiLogoLinkedin className="h-5 w-5" />
         </Link>
-        <Link href="https://www.facebook.com/malekgh.py/" target="_blank">
+        <Link
+          href="https://www.facebook.com/malekgh.py/"
+          target="_blank"
+          aria-label="Facebook"
+        >
           <BiLogoFacebook className="h-5 w-5" />
         </Link>
-        <Link href="https://github.com/FindMalek" target="_blank">
+        <Link
+          href="https://github.com/FindMalek"
+          target="_blank"
+          aria-label="GitHub"
+        >
           <BiLogoGithub className="h-5 w-5" />
         </Link>
-        <Link href="https://www.behance.net/malekpy" target="_blank">
+        <Link
+          href="https://www.behance.net/malekpy"
+          target="_blank"
+          aria-label="Behance"
+        >
           <BiLogoBehance className="h-5 w-5" />
         </Link>
       </div>
 
       <div className="md:hidden fixed z-50 bottom-0 left-0 right-0 bg-cyan-950/50 p-4 flex space-x-16 justify-center">
-        <Link href="https://www.instagram.com/findmalek" target="_blank">
+        <Link
+          href="https://www.instagram.com/findmalek"
+          target="_blank"
+          aria-label="Instagram"
+        >
           <BiLogoInstagram className="h-5 w-5" />
         </Link>
         <Link
           href="https://www.linkedin.com/in/malek-gara-hellal-a2936221a/"
           target="_blank"
+          aria-label="LinkedIn"
         >
           <BiLogoLinkedin className="h-5 w-5" />
         </Link>
-        <Link href="https://www.facebook.com/malekgh.py/" target="_blank">
+        <Link
+          href="https://www.facebook.com/malekgh.py/"
+          target="_blank"
+          aria-label="Facebook"
+        >
           <BiLogoFacebook className="h-5 w-5" />
         </Link>
-        <Link href="https://github.com/FindMalek" target="_blank">
+        <Link
+          href="https://github.com/FindMalek"
+          target="_blank"
+          aria-label="GitHub"
+        >
           <BiLogoGithub className="h-5 w-5" />
         </Link>
-        <Link href="https://www.behance.net/malekpy" target="_blank">
+        <Link
+          href="https://www.behance.net/malekpy"
+          target="_blank"
+          aria-label="Behance"
+        >
           <BiLogoBehance className="h-5 w-5" />
         </Link>
       </div>
