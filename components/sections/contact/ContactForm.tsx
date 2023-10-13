f418835deac0a2c34aeecb169b69b26430b5bc8a
FindMalek <malekgarahellalbus@gmail.com>
2023-10-13 14:23:30 +0100

    🔄 Chore: Code Cleaning.

--- components/sections/contact/ContactForm.tsx
+++ components/sections/contact/ContactForm.tsx
@@ -3,7 +3,6 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import nodemailer from "nodemailer";
 
 import Link from "next/link";
 
@@ -149,7 +148,7 @@ export default function ContactForm() {
           />
           <Button
             type="submit"
-            className="block w-full bg-cyan-800/40 dark:bg-cyan-600/40 font-semibold text-white  hover:text-black dark:hover:text-white shadow-sm hover:bg-cyan-950/20 dark:hover:bg-cyan-600/60"
+            className="block w-full bg-cyan-800/40 dark:bg-cyan-600/40 font-semibold text-white hover:text-black dark:hover:text-white shadow-sm hover:bg-cyan-950/20 dark:hover:bg-cyan-600/60"
           >
             Let's talk
           </Button>
