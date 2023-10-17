5d867d6a0a40675eb8a1cb95a4d84bc73f8749b9
FindMalek <malekgarahellalbus@gmail.com>
2023-10-17 20:27:13 +0100

    📦 Release 0.4.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
 
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 import Header from "@/components/navigation/Header";
 import Background from "@/components/overall/Background";
 
-import { Toaster } from "@/components/ui/Toaster"
+import { Toaster } from "@/components/ui/Toaster";
 
 import type { Metadata } from "next";
 import { poppins } from "@/lib/fonts";
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
     "FindServices | Malek Gara-Hellal - Web Developer, Graphic Designer, and Writer.",
   description:
     "FindServices | Find services near you, anywhere in the world. This is me Malek Gara-Hellal and I am a full-stack developer. I am also a graphic designer and a writer.",
+  applicationName: "https://findservices.tech",
 };
 
 export default function RootLayout({
@@ -24,14 +25,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <meta
+        name="google-site-verification"
+        content="DUd_5XyOU-5LOWSU2CiGLn0U2oEzgGAFJioIy5vl_2o"
+      />
       <body className={poppins.className}>
         <Header />
-  
+
         <div className="mx-auto lg:max-w-4xl md:max-w-2xl max-w-lg xs:max-w-md p-4 pt-8 sm:pt-10 md:pt-10 lg:pl-16">
           {children}
         </div>
         <Toaster />
-        
+
         <Background />
         <Analytics />
       </body>
