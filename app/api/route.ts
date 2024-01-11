283d82ca8ab61ba6b1ab0c66c290e2efcf42c093
FindMalek <malekgarahellalbus@gmail.com>
2024-01-11 18:16:17 +0100

    :lipstick: Updated the Mailer Form.

--- app/api/route.ts
+++ app/api/route.ts
@@ -25,15 +25,67 @@ export async function POST(req: Request) {
 		from: `FindMalek Mailer <` + process.env.SENDER_EMAIL + `>`,
 		to: process.env.MAIN_EMAIL,
 		subject: `[FindMalek] - Contact Form Submission: ${body.name}`,
-		text: `
-      -- Contact Form Submission > FindMalek
-  
-      - Name: ${body.name}
-      - Email: ${body.email}
-      - Phone Number: ${body.phoneNumber}
-      
-      - Message:
-      ${body.message}
+		html: `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              line-height: 1.6;
+            }
+            .container {
+              width: 80%;
+              margin: 20px auto;
+              padding: 20px;
+              border: 1px solid #ddd;
+              border-radius: 5px;
+              background-color: #f9f9f9;
+            }
+            .header {
+              font-size: 20px;
+              color: #333;
+            }
+            .content {
+              margin-top: 20px;
+            }
+            .content p {
+              margin: 5px 0;
+            }
+            .footer {
+              margin-top: 30px;
+              font-size: 14px;
+              text-align: center;
+            }
+            a {
+              color: #007bff;
+              text-decoration: none;
+            }
+            a:hover {
+              text-decoration: underline;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="header">
+              <h2>Contact Form Submission > FindMalek</h2>
+            </div>
+            <div class="content">
+              <p><strong>Full Name:</strong> ${body.name}</p>
+              <p><strong>Email:</strong> ${body.email}</p>
+              <p><strong>Phone Number:</strong> ${body.phoneNumber}</p>
+
+              <h3>Message:</h3>
+              <p>${body.message}</p>
+            </div>
+            <div class="footer">
+              <p>
+			 	This email was sent by FindMalek Mailer. If you have any questions. 
+			  </p>
+            </div>
+          </div>
+        </body>
+      </html>
     `,
 	};
 
