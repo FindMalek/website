b9d6978b62702c9983c9b1821974e63d4c5e673d
FindMalek <hi@findmalek.com>
2024-11-07 13:03:15 +0100

    chore: removed un-published project

--- data/projects.ts
+++ data/projects.ts
@@ -18,7 +18,6 @@ import fn03 from "@/data/projects/fn03";
 import walletLabels from "@/data/projects/wallet-labels";
 import findplate from "@/data/projects/findplate";
 import tallyZero from "@/data/projects/tally-zero";
-import stelify from "@/data/projects/stelify";
 
 export default [
 	uniswaphooks,
@@ -39,5 +38,4 @@ export default [
 	walletLabels,
 	findplate,
 	tallyZero,
-	stelify,
 ];
